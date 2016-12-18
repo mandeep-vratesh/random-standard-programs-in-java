af50e2335d133030ad96cb5db6a75390f68ec6e8
mandeep-vratesh <mandeep.vratesh@gmail.com>
2016-12-19 02:20:06 +0530

    initial commit

---
+++ NearestPrime/src/nearestprime/NearestPrime.java
@@ -0,0 +1,29 @@
+/*
+ * Finds the nearest prime to a number
+ */
+package nearestprime;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author MANDEEP VRATESH
+ */
+public class NearestPrime {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //get the input number
+        int n = sc.nextInt();
+        
+        //calculation of the nearest two primes
+        //6k+1=n || 6k-1=n
+        //k = (n-1)/6 || (n+1)/6
+        
+    }
+    
+}
