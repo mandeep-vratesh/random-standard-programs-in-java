6257a6349181a5ffb5019498fef08bbcb15fdca2
mandeep-vratesh <mandeep.vratesh@gmail.com>
2017-02-17 15:22:12 +0530

    initial commit

---
+++ PDS-experiment-4/src/excercise/pkg1/Excercise1.java
@@ -0,0 +1,37 @@
+/*
+ * A program to convert from IP to symbolic addresses and symbolic to IP addresses.
+ */
+package excercise.pkg1;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Scanner;
+
+/**
+ *
+ * @author MANDEEP VRATESH
+ */
+public class Excercise1 {
+
+    /**
+     * @param args the command line arguments
+     * @throws java.net.UnknownHostException
+     */
+    public static void main(String[] args) throws UnknownHostException {
+        
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        //symbolic to IP
+        InetAddress address = InetAddress.getByName(input);
+        for(byte i : address.getAddress()){
+            System.out.print(i+".");
+        }
+        
+        System.out.println();
+        
+        //IP to symbolic
+//        address = InetAddress.getByAddress(new byte[]{23,66,124,110});
+//        System.out.println(address.getCanonicalHostName());
+    }
+    
+}
